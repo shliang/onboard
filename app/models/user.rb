ff166edc0bb909536cd22db61f8742885ee66257
just this guy <github.com@justthisguy.org>
2012-12-22 21:45:36 -0800

    started omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, 
          :token_authenticatable, :confirmable, 
-         :lockable#, :timeoutable, :omniauthable
+         :lockable, :timeoutable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :admin
