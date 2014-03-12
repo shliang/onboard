1ee3c8fbe1124d1e1ef74cf5f33cb1d657199436
just this guy <github.com@justthisguy.org>
2014-03-11 17:07:10 -0700

    Adds #profiles to User with support for authenticated and non-authenticated users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
+  before_filter :auth_user
 
   def profiles
     @users = User.all
+  end
+
+protected
+  def auth_user
+    if !user_signed_in?
+      redirect_to new_user_session_path, :notice => t(:not_signed_in)
+    end
   end
 
 end
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -1,11 +1,53 @@
 require 'spec_helper'
 
 describe UsersController do
-  let(:users) { [FactoryGirl.create(:user), FactoryGirl.create(:user)] }
-  describe 'users#profiles returns all users' do
-    it 'sets @user to an array of all users' do
-      get :profiles
-      assigns(:users).should eq(users)
+
+  describe 'non-authenticated user' do
+
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      @user.confirm!
+      sign_out @user
+    end
+
+    describe 'users#profiles' do
+      # let(:I18n) { double("I18n") }
+
+      it 'redirects signin with flash message' do
+
+        #### TODO: should decuple test from value in en.yml
+        # puts "~~~~~~~~~~~~~ I18n.t(:not_signed_in)  #{I18n.t(:not_signed_in)}"
+        # # I18n.stub(:not_signed_in) { 'this is my test' }
+        # allow(I18n.t).to receive(:not_signed_in) { 'this is my test' }
+        # I18n.stub(t: :not_signed_in) { "this is my test" }
+
+        # puts ">>>>>>>>>>>>> I18n.t(:not_signed_in)  #{I18n.t(:not_signed_in)}"
+
+        expect(get :profiles).to redirect_to(new_user_session_path)
+        flash[:notice].should eq 'Sorry. Gotta sign in to see that. Please sign in.'
+      end
+
     end
   end
+
+
+  describe 'authenticated user' do
+    let(:test_users) { [FactoryGirl.create(:user), FactoryGirl.create(:user)] }
+
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      @user.confirm!
+      sign_in @user
+    end
+
+    describe 'users#profiles' do
+      it 'sets @user to an array of all users' do
+        test_users << @user
+
+        get :profiles
+        assigns(:users).should =~ test_users
+      end
+    end
+  end
+
 end
